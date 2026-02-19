88314649906b956de1c78c14cc95dff3009b4478
mitugui <cristian.mitugui04@gmail.com>
2026-02-19 14:06:53 -0300

    feat: integrate Nav component into MainLayout for improved navigation

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,8 +1,13 @@
 import { Outlet } from "react-router-dom";
 
+import Nav from "./nav";
+
 function MainLayout() {
   return (
-    <Outlet />
+    <>
+      <Nav />
+      <Outlet />
+    </>
   );
 }
 
