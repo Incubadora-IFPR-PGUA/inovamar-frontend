6e4908b75a6c701c4f19ace75e7eeb01e49cc676
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2026-02-22 22:23:28 -0300

    feat: add footer companies section (#16)

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 
+import Footer from "./footer";
 import Nav from "./nav";
 
 function MainLayout() {
@@ -7,6 +8,7 @@ function MainLayout() {
     <>
       <Nav />
       <Outlet />
+      <Footer />
     </>
   );
 }
