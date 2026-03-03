6edf29781ac7a16ad87a71388ee6f3009e85ebf5
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2026-03-03 14:48:49 -0300

    feat: set navigation bar to be sticky on scroll (#20)

--- src/layouts/nav.tsx
+++ src/layouts/nav.tsx
@@ -2,7 +2,7 @@ import iceLogo from "../assets/img/logos/ice_white.png";
 
 function Nav() {
   return (
-    <nav className="bg-brand-500 text-white px-6 py-6">
+    <nav className="sticky bg-brand-500 text-white px-6 py-6 top-0 left-0">
       <div className="flex items-center gap-3 md:justify-center">
         <img src={iceLogo} alt="Inovamar Incubadora Logo" className="w-13" />
         <h1 className="text-2xl font-bold md:text-3xl">Inovamar</h1>
