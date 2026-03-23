375f87820d4129265c5b358a47e7e1840fdb6b62
mitugui <cristian.mitugui04@gmail.com>
2026-03-23 08:51:00 -0300

    refactor: simplify rendering logic in CallPage component and remove unnecessary ScrollRestoration component from Error and Loading components by adding in CallPage

--- src/components/call/error.tsx
+++ src/components/call/error.tsx
@@ -1,5 +1,3 @@
-import { ScrollRestoration } from "react-router-dom";
-
 import BackButton from "../call/back-button";
 
 type ErrorProps = {
@@ -10,7 +8,6 @@ type ErrorProps = {
 function Error({ status, message }: ErrorProps) {
   return (
     <div className="flex flex-col gap-4 items-center justify-center min-h-[calc(100vh-5rem)] md:min-h-[calc(100vh-4rem)]">
-      <ScrollRestoration />
       <span className="font-black text-8xl">{status}</span>
       <span className="font-black px-3 text-center text-sm md:text-xl">{message}</span>
       <BackButton />
